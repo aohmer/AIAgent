a1b5c3179dd4a81b6bc5cef8c1f6e87894e6a0bb
aohmer <aaron.ohmer@gmail.com>
2025-10-13 15:43:45 -0400

    completed functions

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,6 +1,5 @@
 import os
 
-
 def get_files_info(working_directory, directory="."):
     abs_working_dir = os.path.abspath(working_directory)
     target_dir = os.path.abspath(os.path.join(working_directory, directory))
@@ -21,23 +20,3 @@ def get_files_info(working_directory, directory="."):
         return "\n".join(files_info)
     except Exception as e:
         return f"Error listing files: {e}"
-    
-def get_file_content(working_directory, file_path):
-    abs_working_dir = os.path.abspath(working_directory)
-    target_dir = os.path.abspath(os.path.join(working_directory, file_path))
-    if not target_dir.startswith(abs_working_dir):
-        return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
-    if not os.path.isfile(target_dir):
-       return f'Error: File not found or is not a regular file: "{file_path}"'
-    try:
-        MAX_CHARS = 10000
-        with open(target_dir, "r") as f:
-            file_content_string = f.read(MAX_CHARS)
-            extra = f.read(1)
-            if extra != '':
-                return f'{file_content_string}[...File "{file_path}" truncated at 10000 characters]'
-            else:
-                return file_content_string
-
-    except Exception as e:
-        return f"Error: {e}"
--- tests.py
+++ tests.py
@@ -1,25 +1,61 @@
 from functions.get_files_info import get_files_info
 from functions.get_files_info import get_file_content
+from functions.get_files_info import write_file
+from functions.get_files_info import run_python_file
 
 def test():
-    result = get_file_content("calculator", "main.py")
-    print("Result for main.py")
+    result = run_python_file("calculator", "main.py")
     print(result)
-    print("")
-    result = get_file_content("calculator", "pkg/calculator.py")
-    print("Result for pkg/calculator.py")
+
+    result = run_python_file("calculator", "tests.py")
     print(result)
-    print("")
-    result = get_file_content("calculator", "/bin/cat")
-    print("Result for /bin/cat")
+
+    result = run_python_file("calculator", "../main.py")
     print(result)
-    print("")
-    result = get_file_content("calculator", "pkg/does_not_exist.py")
-    print("Result for pkg/does_not_exist.py")
+
+    result = run_python_file("calculator", "nonexistent.py")
     print(result)
-    print("")
+
+    result = run_python_file("calculator", "lorem.txt")
+    print(result)
+
+    # result = write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum")
+    # print("Result for lorem.txt")
+    # print(result)
+    # print("")
+
+    # result = write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet")
+    # print("Result for pkg/morelorem.txt")
+    # print(result)
+    # print("")
+
+    # result = write_file("calculator", "/tmp/temp.txt", "this should not be allowed")
+    # print("Result for /tmp/temp.txt")
+    # print(result)
+    # print("")
+    # result = get_file_content("calculator", "main.py")
+    # print("Result for main.py")
+    # print(result)
+    # print("")
+
+    # result = get_file_content("calculator", "pkg/calculator.py")
+    # print("Result for pkg/calculator.py")
+    # print(result)
+    # print("")
+
+    # result = get_file_content("calculator", "/bin/cat")
+    # print("Result for /bin/cat")
+    # print(result)
+    # print("")
+
+    # result = get_file_content("calculator", "pkg/does_not_exist.py")
+    # print("Result for pkg/does_not_exist.py")
+    # print(result)
+    # print("")
+
     # result = get_file_content("calculator", "lorem.txt")
     # print(result)
+
     # result = get_files_info("calculator", ".")
     # print("Result for current directory:")
     # print(result)
