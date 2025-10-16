343a687e18e9015160a57dd9630656f228f5f682
aohmer <aaron.ohmer@gmail.com>
2025-10-15 21:14:12 -0400

    added function declarations

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 from google import genai
 import sys
 from google.genai import types
+from functions.get_files_info import schema_get_files_info
 
 messages = [
     types.Content(role="user", parts=[types.Part(text=sys.argv[1])]),
@@ -11,15 +12,32 @@ load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 
 client = genai.Client(api_key=api_key)
+system_prompt = """
+You are a helpful AI coding agent.
 
+When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+- List files and directories
+
+All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+"""
 if sys.argv[1] == None:
     
     print("Prompt is missing.")
     sys.exit(1)
 
+available_functions = types.Tool(
+    function_declarations=[
+        schema_get_files_info,
+    ]
+)
 
 response = client.models.generate_content(
-    model='gemini-2.0-flash-001', contents=messages
+    model='gemini-2.0-flash-001', 
+    contents=messages,
+    config=types.GenerateContentConfig(
+    tools=[available_functions], system_instruction=system_prompt
+)
 )
 
 if len(sys.argv) > 2 and sys.argv[2] == "--verbose":
@@ -27,4 +45,8 @@ if len(sys.argv) > 2 and sys.argv[2] == "--verbose":
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
     print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 else:
-    print(response.text)
+    if len(response.function_calls) > 0:
+        for function_call_part in response.function_calls:
+            print(f"Calling function: {function_call_part.name}({function_call_part.args})")
+        else:
+            print(response.text)
